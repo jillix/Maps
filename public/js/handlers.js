ef00f3aaec3b88511cb00c7e7215a309705cb545
Ionică Bizău <bizauionica@yahoo.com>
2014-04-20 16:44:29 +0300

    Simplified the client side. Added bind-table-crud in app.

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -6,55 +6,11 @@
 
     window.Maps = window.Maps || {};
 
-    // modules
-    var Layout = null
-      , MonoMaps = null
-      , howManyModules = 2
-      , moduleLoadComplete = 0
-      ;
-
-    /*************************
-     *        LAYOUT         *
-     *************************/
-    window.Maps.ready = {
-        /**
-         *  Layout ready
-         *  Layout is ready.
-         *
-         */
-        layout: function () {
-
-            // set layout
-            Layout = this;
-
-            // call module loaded handler
-            moduleLoadedHandler();
-        }
-        /**
-         *  Layout ready
-         *  Layout is ready.
-         *
-         */
-      , mono_maps: function (miid) {
-
-            // set layout
-            MonoMaps = miid;
-
-            // call module loaded handler
-            moduleLoadedHandler();
-        }
-    };
-
     /**
-     *  This function is called every time when a module is
-     *  loaded in page
+     *  Logged in view layout is ready
      *
-     */
-    function moduleLoadedHandler () {
-
-        // not loaded all modules yet
-        if (++moduleLoadComplete !== howManyModules) { return; }
-
+     * */
+    window.Maps.ready = function () {
         console.log("Load complete");
-    }
+    };
 })(window);
