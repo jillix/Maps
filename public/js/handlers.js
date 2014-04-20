53dd79f8c2d32389369c89b5f4d72f4067b53f65
Ionică Bizău <bizauionica@yahoo.com>
2014-04-20 18:32:47 +0300

    Create map form

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -12,6 +12,9 @@
      * */
     window.Maps.ready = function () {
 
+        // load bootstrap js
+        $.getScript ("//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js");
+
         // set table template
         M.miids.maps_table.setTemplate ({
             id: "dummy"
@@ -54,5 +57,69 @@
         M.miids.mono_maps.read({type: "map", query: {}}, function (err, data) {
             M.miids.maps_table.renderItemsFromResult(err, data)
         });
+
+        // create map
+        $(".btn.btn-create-map").on("click", function () {
+
+            // load form
+            M.miids.forms.loadForm ({ formId: "create_map" }, function (err, data) {
+
+                // handle error
+                if (err) {
+                    return console.error (err);
+                }
+
+                // show modal
+                $("#modal").modal("show");
+            });
+        });
     };
+
+    /**
+     *  Form serializer module will call this function
+     *
+     * */
+    window.Maps.dialogSaved = function (formObj) {
+
+        // get the type
+        var type = formObj.formType;
+        delete formObj.formType;
+
+        var data = null;
+        switch (type) {
+            case "map":
+                data = {
+                    name: formObj.name
+                  , options: {
+                        center: {
+                            lat: Number (formObj.lat)
+                          , lng: Number (formObj.lng)
+                        }
+                      , zoom: Number (formObj.zoom)
+                      , type: formObj.type
+                    }
+                }
+                break;
+        }
+
+        // create maps, markers, infowindows, icon
+        M.miids.mono_maps.create({
+            type: type
+          , data: data
+        }, function (err, data) {
+
+            // handle error
+            if (err) {
+                return alert (err);
+            }
+
+            // read maps and render them
+            M.miids.mono_maps.read({type: "map", query: {}}, function (err, data) {
+                M.miids.maps_table.renderItemsFromResult(err, data)
+
+                // show modal
+                $("#modal").modal("hide");
+            });
+        });
+    }
 })(window);
