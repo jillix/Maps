22cc1b3e7bd02e49a6bdc3b5575d7fa57d620d3d
Ionică Bizău <bizauionica@yahoo.com>
2014-04-20 17:26:32 +0300

    Set the table template and populate it with maps

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -11,6 +11,42 @@
      *
      * */
     window.Maps.ready = function () {
-        console.log("Load complete");
+
+        // set table template
+        M.miids.maps_table.setTemplate ({
+            id: "dummy"
+          , options: {}
+          , schema: {
+                name: {
+                    type: 'string'
+                  , label: "Name"
+                  , order: 1
+                }
+              , "options.center.lat": {
+                    type: 'number'
+                  , label: "Lat"
+                  , order: 2
+                }
+              , "options.center.lng": {
+                    type: 'number'
+                  , label: "Lng"
+                  , order: 3
+                }
+              , "options.zoom": {
+                    type: 'number'
+                  , label: "Zoom"
+                  , order: 4
+                }
+              , "options.type": {
+                    type: 'string'
+                  , label: "Type"
+                  , order: 5
+                }
+            }
+        });
+
+        M.miids.mono_maps.read({type: "map", query: {}}, function (err, data) {
+            M.miids.maps_table.renderItemsFromResult(err, data)
+        });
     };
 })(window);
