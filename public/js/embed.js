c2d8d1130fd6457fc24413e1ce4ad5d036d87cc4
Ionică Bizău <bizauionica@yahoo.com>
2014-04-21 15:45:16 +0300

    Added embed page basic configuration

---
+++ public/js/embed.js
@@ -0,0 +1,39 @@
+(function (window) {
+
+    // create the namespace
+    var Maps = {embed: {}};
+
+    /**
+     *  Embed layout is ready
+     *
+     * */
+    Maps.embed.ready = function () {
+
+        var mapId = queryString ("mapId")
+          , $map  = $(".map")
+          , $debugPre = $("pre.debug")
+          ;
+
+        // no map id
+        if (!mapId) {
+            $map.hide();
+            $debugPre.text("Missing mapId.");
+        }
+    };
+
+    /**
+     *  Util query string function
+     *  Thanks: http://stackoverflow.com/a/901144/1420197
+     *
+     * */
+    function queryString (name) {
+        // return Url.queryString(name);
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
+    // set the namespace
+    window.Maps = Maps;
+})(window);
